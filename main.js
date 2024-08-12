d146574824d55692aa021080b73816a73cb1e8d9
Hfzhmkrtwra <hafizahkartawiria701@gmail.com>
2024-08-12 10:29:10 +0700

    update main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDGYnq4VKq-YGu4RbfoI_ZHez9fishYjZo",
+  authDomain: "insan-cemerlang-afd2f.firebaseapp.com",
+  projectId: "insan-cemerlang-afd2f",
+  storageBucket: "insan-cemerlang-afd2f.appspot.com",
+  messagingSenderId: "686649580589",
+  appId: "1:686649580589:web:61374bbbd68adb604eaca4",
+  measurementId: "G-LNZTQBCE26"
+};
